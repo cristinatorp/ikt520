7e66502e1ad122847926d0874db89a8e8713877c
Cristina <cristinavstorp@hotmail.com>
2020-03-12 14:34:43 +0100

    Added lab 4

---
+++ Lab4/emit_logs.py
@@ -0,0 +1,26 @@
+import pika
+
+# Run RabbitMQ:
+# docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3-management
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
+channel = connection.channel()
+
+channel.exchange_declare(exchange="direct_logs", exchange_type="direct")
+
+logs = []
+codes = set()
+
+with open("./Lab4/access.log") as file:
+    for line in file.readlines():
+        logs.append(line.strip())
+
+for log in logs:
+    status_code = log.split(" ")[8]
+    if status_code != "\"-\"" or status_code != "166":
+        codes.add(status_code)
+    channel.basic_publish(exchange="direct_logs", routing_key=status_code, body=log.encode())
+    print(f"Sent log with status code {status_code}")
+
+# print(codes)
+connection.close()
---
+++ Lab4/receive_logs.py
@@ -0,0 +1,63 @@
+import pika
+import sys
+
+# Run RabbitMQ:
+# docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3-management
+
+
+class colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+status_codes = {
+    "200": "OK",
+    "206": "Partial consent",
+    "301": "Permanently moved",
+    "304": "Not modified",
+    "404": "Not found",
+    "502": "Bad gateway"
+}
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
+channel = connection.channel()
+
+channel.exchange_declare(exchange="direct_logs", exchange_type="direct")
+
+result = channel.queue_declare(queue="", exclusive=True)
+queue_name = result.method.queue
+
+# Receive filter info (status code) from the user
+# Can be several (e.g. 206 404 304), one (e.g. 200) or none (includes all)
+user_input = sys.argv[1:]
+if not user_input:
+    for code in status_codes:
+        user_input.append(code)
+
+for code in user_input:
+    channel.queue_bind(exchange="direct_logs", queue=queue_name, routing_key=code)
+
+print(f"Waiting for logs...")
+
+
+def callback(ch, method, properties, body):
+    code = method.routing_key
+    code_color = colors.OKGREEN if code == "200" else (colors.WARNING if code == "206" else colors.FAIL)
+
+    message = body.decode().split(" ")
+    ip, date, time, request, url = message[0], message[3][1:12], message[3][13:], message[5], message[6]
+    # Print date - status code - IP address - url - HTTP method
+    print(f"[{date} {time}] "
+          f"{code_color}[{code}: {status_codes[code].upper()}]"
+          f"{colors.ENDC} Source IP: {colors.HEADER}{colors.BOLD}{ip}{colors.ENDC} | "
+          f"Tried to access {colors.OKBLUE}\"{url}\"{colors.ENDC} ({request[1:]})")
+
+
+channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+channel.start_consuming()
